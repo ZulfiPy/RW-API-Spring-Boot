cf4ebee42caa1b652826372b577f30addb45c7fa
Zulfugar A <zulfugara@Zulfugars-MacBook-Pro.local>
2025-06-23 17:31:16 +0300

    first commit

---
+++ src/main/java/com/rw/RwSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.rw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RwSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RwSpringBootApplication.class, args);
+    }
+
+}
