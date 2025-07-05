10c1cca4fed5c203078e5010928dd5495393ada9
Zulfugar A <zulfugara@Zulfugars-MacBook-Pro.local>
2025-07-05 16:13:11 +0300

    feat: update Driver model with validation constraints

--- src/main/java/com/rw/model/Driver.java
+++ src/main/java/com/rw/model/Driver.java
@@ -41,7 +41,6 @@ public class Driver {
 
     @Column(nullable = false, length = 20, name = "phone_number")
     @NotBlank(message = "Phone number is required")
-//    @Pattern(regexp = "^\\+?[1-9]\\d{1,14}$", message = "Invalid phone number format")
     private String phoneNumber;
 
     @Column(unique = true, nullable = false, length = 100)
