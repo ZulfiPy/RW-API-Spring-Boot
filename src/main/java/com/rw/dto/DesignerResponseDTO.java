310831cb2c30f4271471864b47a06cd182243fb8
Zulfugar A <zulfizupy@gmail.com>
2025-07-06 12:47:10 +0300

    remove: designed related files

--- src/main/java/com/rw/dto/DesignerResponseDTO.java
+++
@@ -1,7 +0,0 @@
-package com.rw.dto;
-
-public record DesignerResponseDTO(
-        Integer id,
-        String name,
-        String designTools
-) {}
--- src/main/java/com/rw/repository/DesignerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rw.repository;
-
-import com.rw.model.Designer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DesignerRepository extends JpaRepository<Designer, Integer> {
-}
