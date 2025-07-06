1b42c5c5d5fbf2a05c56773893655dce8987423d
Zulfugar A <zulfugara@Zulfugars-MacBook-Pro.local>
2025-07-06 12:09:28 +0300

    Use ResponseEntity in DriverController for proper HTTP status

--- src/main/java/com/rw/controller/DriverController.java
+++ src/main/java/com/rw/controller/DriverController.java
@@ -2,15 +2,17 @@ package com.rw.controller;
 
 import com.rw.dto.DriverRequestDTO;
 import com.rw.dto.DriverResponseDTO;
+import com.rw.model.Driver;
 import com.rw.service.DriverService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/drivers")
+@RequestMapping("/api/v1/drivers")
 public class DriverController {
 
     private final DriverService driverService;
@@ -20,13 +22,28 @@ public class DriverController {
     }
 
     @GetMapping
-    public List<DriverResponseDTO> getDrivers() {
-        return driverService.getAllDrivers();
+    public ResponseEntity<List<DriverResponseDTO>> getDrivers() {
+        List<DriverResponseDTO>  drivers = driverService.getAllDrivers();
+        return ResponseEntity.ok(drivers);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DriverResponseDTO> getDriverById(@PathVariable Long id) {
+        DriverResponseDTO driver = driverService.getDriverById(id);
+        return ResponseEntity.ok(driver);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public DriverResponseDTO addDriver(@Valid @RequestBody DriverRequestDTO driverRequestDTO) {
-        return driverService.insertDriver(driverRequestDTO);
+    public ResponseEntity<DriverResponseDTO> addDriver(@Valid @RequestBody DriverRequestDTO driverRequestDTO) {
+        DriverResponseDTO driver = driverService.insertDriver(driverRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(driver);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Void> deleteDriverId(@PathVariable Long id) {
+        driverService.deleteDriverById(id);
+        return ResponseEntity.noContent().build();
     }
 }
