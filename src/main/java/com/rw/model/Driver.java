88aa518897d101af712940527e135fcad166e63d
Zu <zulfizupy@gmail.com>
2025-08-27 08:58:44 +0300

    chore: delete setId

--- src/main/java/com/rw/model/Driver.java
+++ src/main/java/com/rw/model/Driver.java
@@ -91,10 +91,6 @@ public class Driver {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getFirstName() {
         return firstName;
     }
